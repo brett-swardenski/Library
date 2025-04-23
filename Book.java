9aac77f3e39e105a70512cb906731200c70c2c1a
Rebekah Tolliver <rtolliv@iu.edu>
2025-04-23 18:34:11 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,9 +1,12 @@
 /*
- * Class: INFO-C 211
- * Authors:
- * Description: This class is the blueprint for book data.
- */
+ * package LibrarySystem;
+// Project: Library Management System
+// Author: Rebekah Tolliver
+// Date: 23 April 2025
+// This program defines a stub for the Book class with attributes, constructors, 
+// getters, and setters.
 
  public class Book {
+  
     
  }
