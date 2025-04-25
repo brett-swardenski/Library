6f35bd5df29d27206d8330b215fbad928584c82d
brett-swardenski <bswarden@iu.edu>
2025-04-25 19:48:01 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -4,10 +4,29 @@
  * Author: Brett Swardenski
  * Description: This is the primary class of the program that contains the 'main' method and incorporates all other classes.
  */
+import java.util.ArrayList;
 
- public class Library {
+public class Library {
+	// ArrayLists to house book inventory and cardholder information
+   	public ArrayList<Book> books = new ArrayList<>();
+   	public ArrayList<Cardholder> cardholders = new ArrayList<>();
 
-   public static main(String[] args) {
+	// method to find book by title
+	public Book[] findByTitle(String title) {
+		ArrayList<Book> temp = new ArrayList<>();
+		for (Book b: books) {
+			if (b.title = title)
+				temp.add(b);
+		}
+		if (temp[0] = null)
+         	System.out.println("No books found by title: " + title);
+      	return temp;
+	}
+
+	
+
+	// 'main' method to execute the program
+    public static main(String[] args) {
     
-   }
- }
+    }
+}
