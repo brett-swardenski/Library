abcff46ac5c996087083291f609e08499513f1e6
wattsrr <r.watts129@gmail.com>
2025-04-29 10:24:48 -0400

    Update Cardholder.java

--- Cardholder.java
+++ Cardholder.java
@@ -1,10 +1,49 @@
 /*
  * Class: INFO-C 211
- * Authors:  
+ * Authors: Rachel Watts  
  * Description: This class is the blueprint for collecting cardholder information.
  */
 
- public class Cardholder {
-    String custID;
+public class Cardholder {
+  //Attributes
+  String custID; // Cardholder ID 
+  String name; //Cardholder's name
+
+ //Constructor
+ public Cardholder(String custID, String name) {
+  this.custID = custID;
+  this.name = name;
 
  }
+
+// Default constructor 
+public Cardholder() {
+ this.custID = "";
+ this.name = "";
+}
+
+//Getters and setters 
+public String getCustID() {
+ return custID;
+}
+
+public void setCustID( String custID) {
+ this.custID = custID;
+}
+
+ public String getName() {
+  return name;
+ }
+
+ public void setName(String name) {
+  this.name = name;
+ }
+ 
+
+
+
+
+
+
+
+
