81d58f3133deb4fe2a6a1b147b9be8d5d482e64b
brett-swardenski <bswarden@iu.edu>
2025-05-04 16:09:01 -0400

    Update Cardholder.java

--- Cardholder.java
+++ Cardholder.java
@@ -7,45 +7,63 @@
  */
 
 public class Cardholder {
-  //Attributes
-  String custID; // Cardholder ID 
-  String name; //Cardholder's name
+    //Attributes 
+    String memberID; // Cardholder ID 
+    String name; //Cardholder's name
+    Book[] checkedBooks = new Book[Rental.maximumBooks];
 
- //Constructor
- //Sets up a new Cardholder with an ID and name
- public Cardholder(String custID, String name) {
-  this.custID = custID;
-  this.name = name;
+    //Constructor
+    //Sets up a new Cardholder with an ID and name
+    public Cardholder(String memberID, String name) {
+        this.memberID = custID;
+        this.name = name;
+        this.checkedBooks = new Book[Rental.maximumBooks];
+     }
 
- }
+    //Default constructor
+    //Creates a cardholder with empty values
+    public Cardholder() {
+        this.memberID = "";
+        this.name = "";
+        this.checkedBooks = new Book[Rental.maximumBooks];
+    }
 
-//Default constructor
-//Creates a cardholder with empty values
-public Cardholder() {
- this.custID = "";
- this.name = "";
-}
+    //Getter for custID 
+    public String getMemberID() {
+        return memberID;
+    }
+    
+    //Setter for custID
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+    
+    //Getter for name
+    public String getName() {
+        return name;
+    }
+    
+    //Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
 
-//Getter for custID 
-public String getCustID() {
- return custID;
-}
-
-//Setter for custID
-public void setCustID( String custID) {
- this.custID = custID;
-}
-
-//Getter for name
- public String getName() {
-  return name;
- }
-
-//Setter for name
- public void setName(String name) {
-  this.name = name;
- }
-
+    @Override
+    public String toString() {
+        int count = 0;
+        String temp = "Name: " + this.getName() +
+                      "Member ID: " + this.getMemberID() +
+                      "\n" +
+                      "Checked Books: ";
+        for (Book b: checkedBooks) {
+            if (b != null) {
+                temp += b.toString();
+                count++;
+            }
+        }
+        temp += "\n" + count + "/" + checkedBooks.length;
+        return temp;
+    }
 }
 
 
