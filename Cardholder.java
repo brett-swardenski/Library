8afd9b562fb9afd202f800e051969b33af912b86
niraines92 <99304229+niraines92@users.noreply.github.com>
2025-05-07 21:57:34 -0400

    Update Cardholder.java

--- Cardholder.java
+++ Cardholder.java
@@ -1,76 +1,91 @@
+package libraryManagement;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /*
  * Class: INFO-C 211
  * Project: Library Management System
- * Authors: Rachel Watts 
- * Date: April 29, 2025 (Got an extension approved for family emergency)
+ * Author: Rachel Watts
+ * Date: April 29, 2025
  * Description: This class is the blueprint for collecting cardholder information.
  */
 
-public class Cardholder {
-    //Attributes 
-    String memberID; // Cardholder ID 
-    String name; //Cardholder's name
-    Book[] checkedBooks = new Book[Rental.maximumBooks];
+public class Cardholder implements Serializable{
+    // Attributes
+    private String memberID;
+    private String name;
+    private ArrayList<Rental> rentals;
 
-    //Constructor
-    //Sets up a new Cardholder with an ID and name
+    // Constructor
     public Cardholder(String memberID, String name) {
-        this.memberID = custID;
+        this.memberID = memberID;
         this.name = name;
-        this.checkedBooks = new Book[Rental.maximumBooks];
-     }
+        this.rentals = new ArrayList<>();
+    }
 
-    //Default constructor
-    //Creates a cardholder with empty values
+    // Default constructor
     public Cardholder() {
         this.memberID = "";
         this.name = "";
-        this.checkedBooks = new Book[Rental.maximumBooks];
+        this.rentals = new ArrayList<>();
     }
 
-    //Getter for custID 
+    // Getters and Setters
     public String getMemberID() {
         return memberID;
     }
-    
-    //Setter for custID
+
     public void setMemberID(String memberID) {
         this.memberID = memberID;
     }
-    
-    //Getter for name
+
     public String getName() {
         return name;
     }
-    
-    //Setter for name
+
     public void setName(String name) {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        int count = 0;
-        String temp = "Name: " + this.getName() +
-                      "Member ID: " + this.getMemberID() +
-                      "\n" +
-                      "Checked Books: ";
-        for (Book b: checkedBooks) {
-            if (b != null) {
-                temp += b.toString();
-                count++;
+    public ArrayList<Rental> getRentals() {
+        return rentals;
+    }
+    
+    // Determines if the maximum number of books has been reached within the ArrayList for rentals per cardholder. 
+    // Uses the maximumBooks variable from rentals
+    public boolean canCheckoutMoreBooks() {
+        return rentals.size() < Rental.maximumBooks;
+    }
+
+    public void addRental(Rental rental) {
+        if (rental != null) {
+            rentals.add(rental);
+        }
+    }
+
+    public void returnRental(Book book) {
+        Rental target = null;
+        for (Rental r : rentals) {
+            if (r.getRentedBook().equals(book)) {
+                target = r;
+                break;
             }
         }
-        temp += "\n" + count + "/" + checkedBooks.length;
-        return temp;
+        if (target != null) {
+            rentals.remove(target);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder cardholderString = new StringBuilder();
+        cardholderString.append("Name: ").append(name).append("\n")
+            .append("Member ID: ").append(memberID).append("\n")
+            .append("Current Rentals (").append(rentals.size()).append("/").append(Rental.maximumBooks).append("):\n");
+        for (Rental r : rentals) {
+            cardholderString.append("- ").append(r.getRentedBook().getTitle()).append("\n");
+        }
+        return cardholderString.toString();
     }
 }
-
-
-
-
-
-
-
-
-
