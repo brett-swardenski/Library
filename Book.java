30215ad2438ef93d3a6ad4504396081918b7629e
niraines92 <99304229+niraines92@users.noreply.github.com>
2025-05-07 21:57:15 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,39 +1,47 @@
+package libraryManagement;
+
+import java.io.Serializable;
+
 /*
 // Project: Library Management System
 // Author: Rebekah Tolliver
 // Date: 23 April 2025
 // This program defines the Book class with attributes, constructors, 
 // getters, setters, a variable linking to the Author class, and a toString method.
+ */
 
- public class Book {
-  
-       //Attributes  
-       private String title;
-       private String isbn;
-       private boolean isBorrowed;
-       private Author author;
+public class Book implements Serializable{
 
-      // Default constructor
-      // Initializes all attributes to empty strings
-      public Book() {
+    //Attributes  
+    private String title;
+    private String isbn;
+    private boolean isBorrowed;
+    private Author author;
+    private Book rentedBook;
+    private Rental rental;
+
+    // Default constructor
+    // Initializes all attributes to empty strings
+    public Book() {
         this.title = "";
         this.isbn = "";
         this.isBorrowed = false;
         this.author = null;
-      }
-  }
-  
-  // Parameterized constructor
-  // Initializes the Book with title, isbn, and if it borrowed
-  public Book(String title, String isbn, Author author) {
+        this.rental = null;
+    }
+
+    // Parameterized constructor
+    // Initializes the Book with title, isbn, and if it borrowed
+    public Book(String title, String isbn, Author author) {
         this.title = title;
         this.isbn = isbn;
         this.isBorrowed = false;
         this.author = author;
+        this.rental = null;
     }
-  
-  //Getter and Setter Methods
-  public String getTitle() {
+
+    //Getter and Setter Methods
+    public String getTitle() {
         return title;
     }
 
@@ -56,23 +64,30 @@
     public void setBorrowed(boolean borrowed) {
         isBorrowed = borrowed;
     }
-  
-    public void getAuthor() {
+
+    public Author getAuthor() {
         return author;
     }
-  
+
     public void setAuthor(Author author) {
         this.author = author;
+    }
+    
+    public Rental getRental() {
+        return rental;
+    }
+    
+    public void setRental(Rental rental) {
+        this.rental = rental;
     }
 
     // Returns a string representation of the Book object.
     @Override
     public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", isBorrowed=" + isBorrowed +
-                ", author='" + author + '\'' +
-                '}';
+        return  "======="
+                + "\n   Title: " + title 
+                + "\n   ISBN: " + isbn
+                + "\n   Is Borrowed: " + isBorrowed
+                + "\n   Author: " + (author != null ? author.getName() : "N/A");
     }
-  
+}
