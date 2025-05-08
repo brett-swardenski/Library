0b782bda80f8213a5b9dc4ff08de50676d676fae
niraines92 <99304229+niraines92@users.noreply.github.com>
2025-05-07 21:58:11 -0400

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,102 +1,193 @@
 /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package libraryManagement;
+
+/*
  * Project: Library Management System
  * Class: INFO-C 211
- * Author: Brett Swardenski
+ * Author: Brett Swardenski and Nicholas Raines
  * Description: This is the primary class of the program that contains the 'main' method and incorporates all other classes.
  *
  *
  * 4/25/2025: Being the main class of the program, it depends very heavily on the other classes being fully developed.
  *			  
  */
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Library {
-	// ArrayLists to house book inventory and cardholder information
-   	public ArrayList<Book> books = new ArrayList<>();
-   	public ArrayList<Cardholder> cardholders = new ArrayList<>();
-	public ArrayList<Author> authors = new ArrayList<>();
+    // ArrayLists to house book inventory and cardholder information
 
-	// Constructor
-	public Library() {
-		books = new ArrayList<>();
-		cardholders = new ArrayList<>();
-		authors = new ArrayList<>();
-	}
+    public ArrayList<Book> books = new ArrayList<>();
+    public ArrayList<Cardholder> cardholders = new ArrayList<>();
+    public ArrayList<Author> authors = new ArrayList<>();
+    public ArrayList<Rental> rentals = new ArrayList<>();
+    public static final String Save_File = "library_save.dat";
 
-	// method to display books within an ArrayList
-	public void displayBooks(ArrayList<Book> bookList) {
-		for (Book b: bookList) {
-			System.out.println(b.toString());
-		}
-	}
+    // Constructor
+    public Library() {
+        this.books = new ArrayList<>();
+        this.cardholders = new ArrayList<>();
+        this.authors = new ArrayList<>();
+        this.rentals = new ArrayList<>();
+    }
 
-	// method to find book by title
-	public ArrayList<Book> findByTitle(String title) {
-		ArrayList<Book> temp = new ArrayList<>();
-		for (Book b: books) {
-			int comparison = title.compareTo(b.title);
-			if (comparison == 0)
-				temp.add(b);
-		}
-		if (temp[0] = null)
-         	System.out.println("No books found by title: " + title);
-      	return temp;
-	}
+    // Method to display books within an ArrayList
+    public void displayBooks(ArrayList<Book> bookList) {
+        for (Book b : bookList) {
+            System.out.println(b.toString());
+        }
+    }
 
-	public ArrayList<Book> findByAuthor(Author author) {
-		return author.books;
-	}
+    /*
+     *              Book Methods
+     */
+    // Method to add a book to the book arraylist 
+    public void addBook(Book book) {
+        books.add(book);
+        Author author = book.getAuthor();
+        if (findAuthorByName(author.getName()) == null) {
+            addAuthor(author);
+        }
+        author.addBook(book);
+    }
 
-	// method to add a book to the 'books' arraylist and checks if the author is
-	// in the 'authors' arraylist. If not, it adds the author to it.
-	public void addBook(Book book) {
-		books.add(book);
-		Author authorCheck = book.author;
-		if (findAuthor(authorCheck) == null)
-			addAuthor(authorCheck);
-	}
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
 
-	// method to display cardholder information
-	public void displayCardholder(Cardholder c) {
-		System.out.println(c.toString());
-	}
+    public ArrayList<Book> findByTitle(String title) {
+        ArrayList<Book> matches = new ArrayList<>();
+        for (Book b : books) {
+            if (b.getTitle().equalsIgnoreCase(title)) {
+                matches.add(b);
+            }
+        }
+        return matches;
+    }
 
-	// method to display all cardholder information
-	public void displayAllCardholders() {
-		for (Cardholder c: cardholders) {
-			displayCardholder(c);
-		}
-	}
+    public ArrayList<Book> findByAuthor(Author author) {
+        return author != null ? author.getBooks() : new ArrayList<>();
+    }
 
-	// methods to search for particular cardholder
-	public Cardholder findCHbyName(String name) {
-		for (Cardholder c: cardholders) {
-			int comparison = name.compareTo(c.name);
-			if (comparison == 0)
-				return c;
-		}
-		return null;
-	}
+    /*
+     *              Cardholder Methods
+     */
+    public void addCardholder(Cardholder cardholder) {
+        cardholders.add(cardholder);
+    }
 
-	public Cardholder findCHbyPhoneNumber(String phoneNumber) {
-		for (Cardholder c: cardholders) {
-			int comparison = phoneNumber.compareTo(c.phoneNumber);
-			if (comparison == 0)
-				return c;
-		}
-		return null;
-	}
+    public ArrayList<Cardholder> getCardholders() {
+        return cardholders;
+    }
 
-	public Author findAuthor(Author author) {
-		for (Author a: authors) {
-			int comparison = author.compareTo(a);
-			if (comparison == 0) 
-				return a;
-		}
-		return null;
-	}
+    public Cardholder findCardholderByName(String name) {
+        for (Cardholder c : cardholders) {
+            if (c.getName().equalsIgnoreCase(name)) {
+                return c;
+            }
+        }
+        return null;
+    }
 
-	public void addAuthor(Author author) {
-		authors.add(author);
-	}
+    public Cardholder findCardholderByID(String id) {
+        for (Cardholder c : cardholders) {
+            if (c.getMemberID().equals(id)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    /*
+     *              Author methods
+     */
+    public void addAuthor(Author author) {
+        authors.add(author);
+    }
+
+    public ArrayList<Author> getAuthors() {
+        return authors;
+    }
+
+    public Author findAuthorByName(String name) {
+        for (Author a : authors) {
+            if (a.getName().equalsIgnoreCase(name)) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    /*
+     *              Rental Methods
+     */
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public ArrayList<Rental> getRentals() {
+        return rentals;
+    }
+
+    public Rental findRental(Book book, Cardholder cardholder) {
+        for (Rental r : rentals) {
+            if (r.getCardholder().equals(cardholder) && r.getReturnDate() == null && book.equals(r.getRentedBook())) {
+                return r;
+            }
+        }
+        return null;
+    }
+
+    public void removeRental(Rental rental) {
+        rentals.remove(rental);
+    }
+    
+    // Returns a book and displays a message for late fees, if necessary
+    public void returnBook(Book book, Cardholder cardholder) {
+        Rental rental = findRental(book, cardholder);
+        if (rental != null) {
+            LocalDate today = LocalDate.now();
+            rental.setReturnDate(today);
+            rental.calculateLateFee();
+            book.setBorrowed(false);
+            book.setBorrowed(false);
+
+            removeRental(rental);
+            cardholder.returnRental(book);
+
+            System.out.println("Thank you for returning our book.");
+            if (rental.getLateFee() > 0) {
+                System.out.printf("Late fee: $%.2f%n", rental.getLateFee());
+            }
+        } else {
+            System.out.println("Rental not found or the book has already been returned.");
+        }
+    }
+    
+    // Saves the serializable classes so that the current state of the program can be reloaded
+    public void saveToFile() throws IOException {
+        SystemSave saveData = new SystemSave(books, cardholders, rentals);
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(Save_File))) {
+            oos.writeObject(saveData);
+        }
+    }
+    
+    // Loads the previously saved state of the serializable classes
+    public void loadFromFile() throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(Save_File))) {
+            SystemSave saveData = (SystemSave) ois.readObject();
+
+            this.books = saveData.getBooks() != null ? saveData.getBooks() : new ArrayList<>();
+            this.cardholders = saveData.getCardholders() != null ? saveData.getCardholders() : new ArrayList<>();
+            this.rentals = saveData.getRentals() != null ? saveData.getRentals() : new ArrayList<>();
+        }
+    }
 }
